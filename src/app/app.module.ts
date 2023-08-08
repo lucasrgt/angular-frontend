25ac6e697abf772f82690cb379c5718d57cfbfd7
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-08 20:25:09 -0300

    feat: create and configure home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { SearchProductComponent } from './presentation/shared/components/search-product/search-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchProductComponent
   ],
   imports: [
     BrowserModule,
