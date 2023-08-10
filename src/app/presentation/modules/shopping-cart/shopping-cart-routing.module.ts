64b07693bfd654614bb215ffbfa7362c3e765341
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-10 12:03:09 -0300

    feat: add success page to shopping cart module

--- src/app/presentation/modules/shopping-cart/shopping-cart-routing.module.ts
+++ src/app/presentation/modules/shopping-cart/shopping-cart-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
+import { SuccessComponent } from './pages/success/success.component';
 
 const routes: Routes = [
   {
     path: '',
     component: ShoppingCartComponent,
+  },
+  {
+    path: 'success',
+    component: SuccessComponent,
   },
 ];
 
--- src/app/presentation/modules/shopping-cart/shopping-cart.module.ts
+++ src/app/presentation/modules/shopping-cart/shopping-cart.module.ts
@@ -4,9 +4,10 @@ import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.compo
 import { ShoppingCartRoutingModule } from './shopping-cart-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { SuccessComponent } from './pages/success/success.component';
 
 @NgModule({
-  declarations: [ShoppingCartComponent],
+  declarations: [ShoppingCartComponent, SuccessComponent],
   imports: [CommonModule, SharedModule, ShoppingCartRoutingModule, FormsModule],
 })
 export class ShoppingCartModule {}
