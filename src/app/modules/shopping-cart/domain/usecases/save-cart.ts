c09f524cc3b1f58e463cee60309d4f5b948c60c6
Lucas Richard Garcia Tinoco <lucas.tinoco@hotmail.com>
2023-08-11 21:22:06 -0300

    fix: add @Injectable to usecases

--- src/app/modules/shopping-cart/domain/usecases/save-cart.ts
+++ src/app/modules/shopping-cart/domain/usecases/save-cart.ts
@@ -1,8 +1,11 @@
 import { ShoppingCartRepository } from '../repositories/shopping-cart-repository';
 import { ShoppingCartItem } from '../models/cart-item';
-import { Inject } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { SHOPPING_CART_REPOSITORY_TOKEN } from '../../data/tokens/shopping-cart.tokens';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class SaveCart {
   constructor(
     @Inject(SHOPPING_CART_REPOSITORY_TOKEN)
